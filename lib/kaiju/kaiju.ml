af5ca51375c42935091bdb7c092ad81c3453611e
Malcolm Matalka <malcolm.matalka@klarna.com>
2014-06-16 20:07:25 +0200

    Add support for creating loggers and initial parsing of log file

--- lib/kaiju/kaiju.ml
+++ lib/kaiju/kaiju.ml
@@ -1,37 +1,143 @@
 open Core.Std
 open Async.Std
 
-type s = { log : Zolog_std_event.t Zolog.t }
+type s = { log : Zolog_std_event.t Zolog.t; konfig : Konfig.t }
 
-let start_log () =
+module Flag = struct
+  open Command.Spec
+
+  let config_file () =
+    flag "-c" ~doc:" Path to config file"
+      (required string)
+end
+
+let logging = "logging"
+
+let level_of_string = function
+  | "debug"    -> Some Zolog_std_event.Log.Debug
+  | "info"     -> Some Zolog_std_event.Log.Info
+  | "warning"  -> Some Zolog_std_event.Log.Warning
+  | "error"    -> Some Zolog_std_event.Log.Error
+  | "critical" -> Some Zolog_std_event.Log.Critical
+  | _          -> None
+
+let create_console_handler konfig name =
+  let read_options =
+    let open Option.Monad_infix in
+    Konfig.get [logging; name; "min_level"] konfig >>= fun min_level_s ->
+    Konfig.get [logging; name; "max_level"] konfig >>= fun max_level_s ->
+    level_of_string min_level_s                    >>= fun min_level ->
+    level_of_string max_level_s                    >>= fun max_level ->
+    Some (min_level, max_level)
+  in
+  match read_options with
+    | Some (min_level, max_level) -> begin
+      Zolog_std_event_console_backend.create
+        (Zolog_std_event_writer_backend.default_formatter ~min_level ~max_level)
+      >>= fun backend ->
+      let handler = Zolog_std_event_console_backend.handler backend in
+      Deferred.return (Some handler)
+    end
+    | None ->
+      Deferred.return None
+
+let create_file_handler konfig name =
+  let read_options =
+    let open Option.Monad_infix in
+    Konfig.get [logging; name; "min_level"] konfig >>= fun min_level_s ->
+    Konfig.get [logging; name; "max_level"] konfig >>= fun max_level_s ->
+    Konfig.get [logging; name; "dir"] konfig       >>= fun dir ->
+    level_of_string min_level_s                    >>= fun min_level ->
+    level_of_string max_level_s                    >>= fun max_level ->
+    Some (min_level, max_level, dir)
+  in
+  match read_options with
+    | Some (min_level, max_level, dir) -> begin
+      Zolog_std_event_file_backend.create
+        ~formatter:(Zolog_std_event_writer_backend.default_formatter ~min_level ~max_level)
+        (Zolog_std_event_file_backend.default_filename ~dir:dir)
+      >>= fun backend ->
+      let handler = Zolog_std_event_file_backend.handler backend in
+      Deferred.return (Some handler)
+    end
+    | None ->
+      Deferred.return None
+
+
+let create_handler konfig name =
+  match Konfig.get [logging; name; "type"] konfig with
+    | Some "console" -> create_console_handler konfig name
+    | Some "file"    -> create_file_handler konfig name
+    | Some _         -> Deferred.return None
+    | None           -> Deferred.return None
+
+let create_handlers konfig =
+  match Konfig.get [logging; "loggers"] konfig with
+    | Some loggers_raw -> begin
+      let loggers =
+        List.filter
+          ~f:(fun s -> not (String.is_empty s))
+          (String.split ~on:' ' loggers_raw)
+      in
+      Deferred.List.map
+        ~f:(create_handler konfig)
+        loggers
+      >>= fun maybe_handlers ->
+      match Option.all maybe_handlers with
+        | Some handlers ->
+          Deferred.return handlers
+        | None ->
+          (* TODO: Make error handling better here *)
+          failwith "Bad logger setup"
+    end
+    | None ->
+      Deferred.return []
+
+
+let start_log konfig =
   Zolog.start ()
   >>= fun logger ->
-  Zolog_std_event_console_backend.create
-    Zolog_std_event_writer_backend.default_formatter
-  >>= fun backend ->
-  let handelr = Zolog_std_event_console_backend.handler backend in
-  Zolog.add_handler logger handelr
+  create_handlers konfig
+  >>= fun handlers ->
+  Deferred.List.map ~f:(Zolog.add_handler logger) handlers
   >>= fun _ ->
   Deferred.return logger
 
-let create_state () =
-  start_log ()
+let create_state config_file =
+  let config = In_channel.read_all config_file in
+  let konfig = Result.ok_or_failwith (Konfig.parse_string config) in
+  start_log konfig
   >>= fun log ->
-  Deferred.return { log }
+  Deferred.return { log; konfig }
 
-let main () =
-  create_state ()
+let run_start config_file =
+  create_state config_file
   >>= fun state ->
   Zolog_event.info
     ~n:["kaiju"; "main"; "started"]
     ~o:"kaiju_main"
     state.log
-    "Kaiju successfully started"
+    "Kaiju started"
   >>= fun _ ->
   Zolog.sync state.log
   >>= fun _ ->
   Deferred.return (Ok (shutdown 0))
 
-let () =
-  ignore (main ());
-  never_returns (Scheduler.go ())
+let start_cmd = Command.basic
+  ~summary:"Start an instance"
+  Command.Spec.(empty
+                +> Flag.config_file ())
+  (fun config_file () ->
+    ignore (run_start config_file);
+    never_returns (Scheduler.go ()))
+
+let main () =
+  Random.self_init ();
+  Exn.handle_uncaught
+    ~exit:true
+    (fun () ->
+      Command.run ~version:"1.0" ~build_info:"N/A"
+        (Command.group ~summary:"kaiju commands"
+           [ ("start", start_cmd) ]))
+
+let () = main ()
